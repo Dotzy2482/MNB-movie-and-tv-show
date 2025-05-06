29219c64d6fbc00e0955a95fd280b32c41fd4acf
Dotzy2482 <158623821+Dotzy2482@users.noreply.github.com>
2025-05-06 14:39:45 +0300

    Delete loading.js

--- loading.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-    const loader = document.getElementById("loading-screen");
-    loader.style.opacity = "0";
-    setTimeout(() => {
-      loader.style.display = "none";
-      document.querySelector(".page-content").style.display = "block";
-    }, 600); 
-  });
