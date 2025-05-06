6a0016665404bcab9ac27093c1f7f1ae66c7bc85
Dotzy2482 <158623821+Dotzy2482@users.noreply.github.com>
2025-05-06 14:40:03 +0300

    Delete search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-function toggleClearIcon() {
-    const input = document.getElementById('search_form');
-  }
-  
-  function clearSearch() {
-    const input = document.getElementById('search-bar');
-    input.value = '';
-    toggleClearIcon();
-  }
