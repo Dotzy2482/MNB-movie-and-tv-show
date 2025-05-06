888a5dc4cf3101532ca053f48f0a870573f99d94
Dotzy2482 <158623821+Dotzy2482@users.noreply.github.com>
2025-05-06 14:38:47 +0300

    Delete footer.js

--- footer.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.person img').forEach(img => {
-    img.addEventListener('mouseenter', () => {
-        img.style.transform = 'rotate(360deg)';
-    });
-    img.addEventListener('mouseleave', () => {
-        img.style.transform = 'rotate(0deg)';
-    });
-});
